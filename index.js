8464d36fa17a61bf30589bb52167249a806c3be3
“Jared <drueco03@gmail.com>
2022-08-25 15:46:29 -0600

    trying to fetch frontend

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-const { response, request } = require('express');
 const express = require('express');
 const app = express();
 
 const cors = require('cors');
 app.use(cors());
-
-app.use(express.static('build'));
 
 var morgan = require('morgan');
 
@@ -33,6 +30,7 @@ let persons = [
 ];
 
 app.use(express.json());
+app.use(express.static('build'));
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>');
